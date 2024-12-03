484e7a7f995368b74971c695a70b412fa1948c7f
user <user@DESKTOP-DV6KRKO>
2024-12-03 18:54:00 +0200

    Добавьте файлы проекта.

---
+++ labfivetwo/Program.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    Console.WriteLine("Введите предложение");
+    string x = Console.ReadLine();
+    string result = delete(x);
+    Console.WriteLine(result);
+    static string delete (string str)
+    {
+        return str[0] + str[^1].ToString();
+    }
+}
+catch
+{
+    Console.WriteLine("ошмбочка 404....");
+}
